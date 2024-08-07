0c3d29d917397a006803ba8d6977793b89f108cb
tarungampala <48049736+gampalatarun@users.noreply.github.com>
2024-08-07 19:41:49 +0530

    Update app.js api3 is updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const sqlite3 = require('sqlite3')
 const path = require('path')
 const dbpath = path.join(__dirname, 'covid19India.db')
 
-module.exports
+module.exports = app
 
 let db = null
 
@@ -17,7 +17,7 @@ const instalizeDataserver = async () => {
       driver: sqlite3.Database,
     })
 
-    app.listen(30000, () => {
+    app.listen(3000, () => {
       console.log('Server Running at http://localhost:3000/')
     })
   } catch (e) {
@@ -50,8 +50,31 @@ const convertsnaketoCamleCaseDistrictDetails = db => {
 //API1
 
 app.get('/states/', async (request, response) => {
-  const getststesQuery = `select * from state;`
+  const getstatesQuery = `select * from state ORDER BY state_id;`
 
-  const getstateDetails = await db.all(getststesQuery)
-  response.send(getstateDetails)
+  const statesArray = await db.all(getstatesQuery)
+  const convertedStateDetails = statesArray.map(eachstate =>
+    convertsnaketoCamelCaseStateDetails(eachstate),
+  )
+
+  response.send(convertedStateDetails)
+})
+
+//API2
+
+app.get('/states/:stateId/', async (request, response) => {
+  const {stateId} = request.params
+  const getstateQuery = `SELECT * FROM state WHERE state_id=${stateId};`
+  const stateDetails = await db.get(getstateQuery)
+
+  response.send(convertsnaketoCamelCaseStateDetails(stateDetails))
+})
+
+//API3
+
+app.post('/districts/', async (request, response) => {
+  const {districtName, stateId, cases, cured, active, deaths} = request.body
+  const postDistrictQuery = ` INSERT INTO director(district_name,state_id,cases,cured,active,deaths) VALUES('${districtName}',${stateId},${cases},${cured},${active},${deaths})`
+  await db.run(postDistrictQuery)
+  response.send('District Successfully')
 })
